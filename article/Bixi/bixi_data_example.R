5acc186f7905ed06153cc96876aed8dbb6ef9e31
khaled Fouda <kfouda@pm.me>
2026-04-20 15:52:42 -0400

    .

--- article/Bixi/bixi_data_example.R
+++ article/Bixi/bixi_data_example.R
@@ -1,5 +1,5 @@
-source("./article/bixi/data_bixi.R")
-source("./article/bixi/helpers_bixi.R")
+source("./article/Bixi/data_bixi.R")
+source("./article/Bixi/helpers_bixi.R")
 library(dplyr)
 library(magrittr)
 dat <- prepare_bixi_data()
@@ -8,9 +8,11 @@ Z <- dat$Z %>% as.matrix()
 Y <- dat$modd$Y
 kernels <- generate_similarity_bixi(0.25, "Feb_last", prefix = "1",
                                     train_prefix = "65",
-                                    "./article/bixi/data/splits2",return_distance = TRUE)
+                                    "./article/bixi/data/splits",
+                                    return_distance = TRUE)
 
-bixi_example <- list(Y=as.matrix(Y), X=X,Z=Z, kernels = kernels, test = as.matrix(dat$test),
+bixi_example <- list(Y=as.matrix(Y), X=X,Z=Z,
+                     test = as.matrix(dat$test),
                      spatial_distance = kernels$distance$spatial,
                      temporal_positions = kernels$distance$temporal)
-usethis::use_data(bixi_example, overwrite = TRUE)
+usethis::use_data(bixi_example, overwrite = TRUE, compress = "xz")
